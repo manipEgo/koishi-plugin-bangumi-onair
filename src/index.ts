78f14f66ae3bc38ae85d91ce691c0377cd5f8190
manip_ego <52987267+manipEgo@users.noreply.github.com>
2023-12-29 00:32:42 +0800

    feat: get bangumi today with bangumi-data

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
 import { Context, Schema } from 'koishi'
 
+// bangumi-calc
+import { getTodayBangumiData } from './utils/data-calc';
+
+const moment = require('moment');
+
 export const name = 'bangumi-onair'
 
 export interface Config {}
@@ -7,5 +12,10 @@ export interface Config {}
 export const Config: Schema<Config> = Schema.object({})
 
 export function apply(ctx: Context) {
-  // write your plugin here
+  // bangumi onair
+  ctx.command('tv').action((_) => {
+    const timeNow = moment();
+    const bangumi = getTodayBangumiData(timeNow);
+    return JSON.stringify(bangumi.map((b) => b.title)).replaceAll(',', '\n').replaceAll('[', '').replaceAll(']', '');
+  });
 }
---
+++ src/utils/data-calc.ts
@@ -0,0 +1,94 @@
+import bangumiData from 'bangumi-data';
+import moment from 'moment';
+
+/**
+ * Get the season of the time
+ * @param timeNow current time
+ * @returns day 1 of the season
+ */
+const getSeason = (timeNow: moment.Moment) => {
+    const year = timeNow.year();
+    const month = timeNow.month();
+    if (month >= 0 && month <= 2) {
+        return moment([year, 0]);
+    } else if (month >= 3 && month <= 5) {
+        return moment([year, 3]);
+    } else if (month >= 6 && month <= 8) {
+        return moment([year, 6]);
+    } else if (month >= 9 && month <= 11) {
+        return moment([year, 9]);
+    }
+    return moment([year, 0]);
+}
+
+/**
+ * Get the bangumi data of the season
+ * @param timeNow current time
+ * @returns bangumi data of the season
+ */
+const getSeasonBangumiData = (timeNow: moment.Moment) => {
+    const season = getSeason(timeNow);
+    const seasonBangumiData = bangumiData.items.filter((b) => {
+        const begin = moment(b.begin);
+        const end = moment(b.end);
+        return (
+            (begin.isSameOrAfter(season) &&
+                end.isSameOrBefore(season.clone().add(3, "months"))) ||
+            b.end === "" ||
+            b.end === undefined
+        );
+    });
+    return seasonBangumiData;
+}
+
+
+/**
+ * Check if the time hit the period
+ * @param timeNow current time
+ * @param begin the begin time of the period
+ * @param period the period
+ * @returns if the time hit the period
+ */
+const checkPeriodHitDay = (timeNow: moment.Moment, begin: moment.Moment, period: moment.Duration) => {
+    if (period.asMilliseconds() === 0) {
+        return timeNow.dayOfYear() === begin.dayOfYear();
+    }
+    const happen = begin.clone();
+    while (happen.isBefore(timeNow)) {
+        happen.add(period);
+    }
+    return timeNow.dayOfYear() === happen.dayOfYear();
+}
+
+
+/**
+ * Get the bangumi data of today
+ * @param timeNow current time
+ * @returns bangumi data of today
+ */
+const getTodayBangumiData = (timeNow: moment.Moment) => {
+    const seasonBangumiData = getSeasonBangumiData(timeNow);
+    const todayBangumiData = seasonBangumiData.filter((b) => {
+        // check if the bangumi is already end
+        if (b.end !== "" && b.end !== undefined) {
+            const end = moment(b.end);
+            if (end.isBefore(timeNow)) {
+                return false;
+            }
+        }
+        const begin = moment(b.begin);
+        // use bangumi-data broadcast for period if exist
+        // https://github.com/bangumi-data/bangumi-data/blob/master/CONTRIBUTING.md#%E5%85%B3%E4%BA%8Ebroadcast%E5%AD%97%E6%AE%B5
+        const broadcast = b.broadcast;
+        // default period for tv and web is 1 week
+        // default period for others is 0 day
+        const period = broadcast ?
+            moment.duration('P' + broadcast.split('P')[1]) :
+            ((b.type === "tv" || b.type === "web") ?
+                moment.duration(1, "week") : moment.duration(0, "day"));
+        return checkPeriodHitDay(timeNow, begin, period);
+    });
+    return todayBangumiData;
+}
+
+export { getTodayBangumiData }
