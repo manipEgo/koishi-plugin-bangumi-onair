6b3d9b3697f6b88545f807b95d6fe34a301a8739
manip_ego <52987267+manipEgo@users.noreply.github.com>
2023-12-29 01:06:56 +0800

    chore: simple output formatter

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import { Context, Schema } from 'koishi'
 
+import moment from 'moment';
+
 // bangumi-calc
 import { getTodayBangumiData } from './utils/data-calc';
-
-const moment = require('moment');
 
 export const name = 'bangumi-onair'
 
@@ -16,6 +16,20 @@ export function apply(ctx: Context) {
   ctx.command('tv').action((_) => {
     const timeNow = moment();
     const bangumi = getTodayBangumiData(timeNow);
-    return JSON.stringify(bangumi.map((b) => b.title)).replaceAll(',', '\n').replaceAll('[', '').replaceAll(']', '');
+    bangumi.sort((a, b) => {
+      return moment(a.begin).format('HH:mm') > moment(b.begin).format('HH:mm') ? 1 : -1;
+    });
+    const bangumiStringList = bangumi.map((b) => moment(b.begin).format('HH:mm') + "   " + b.title);
+    // mark current time
+    let i = 0;
+    while (i < bangumiStringList.length) {
+      if (moment(bangumi[i].begin).format('HH:mm') > moment().format('HH:mm')) {
+        break;
+      }
+      i++;
+    }
+    const timeMarker = "> --- " + moment().format('HH:mm') + " ---\n";
+    const bangumiString = bangumiStringList.slice(0, i).join('\n') + '\n' + timeMarker + bangumiStringList.slice(i).join('\n');
+    return bangumiString;
   });
 }
